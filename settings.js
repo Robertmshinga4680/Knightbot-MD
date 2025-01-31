b74d046006fe806df032933ada37ebd260f6cd5c
Robertmshinga4680 <141998909+Robertmshinga4680@users.noreply.github.com>
2025-01-31 19:30:28 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Mshinga_4680 Bot",
+  botOwner: 'Robert', // Your name
+  ownerNumber: '255756983995', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
